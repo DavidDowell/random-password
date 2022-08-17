b0adc810d6a4694c401df58d38a79c41a9d5f3f8
DavidDowell <daviddowell1@gmail.com>
2022-08-17 16:35:03 -0400

    added loop for choices and created functions for the loop to push the variables in the loop

--- script.js
+++ script.js
@@ -13,15 +13,15 @@ function writePassword() {
 function generatePassword() {
 
     //asks for number of characters from the user
-    let click = parseInt(prompt("How many characters would you like your password to be? It must range from 8-128."));
-    let count = [];
+    let start = parseInt(prompt("How many characters would you like your password to be? It must range from 8-128."));
+    let choices = [];
 
     //if no valid number is chosen and entered at the start
-    if(!click) {
+    if (!start) {
         alert('You need to input a valid number.')
-
-    } else if (click < 8 || click > 128){
-        click = parseInt(prompt('The range is from 8 to 128 characters.'))
+    
+    } else if (start < 8 || start > 128) {
+        start = parseInt(prompt('The range is from 8 to 128 characters.'))
     } else {
         if (confirm('Would you like to include numbers?')) {
             choices.push(randomNumber);
@@ -32,20 +32,49 @@ function generatePassword() {
         if (confirm('Would you like to include upper case letters?')) {
             choices.push(randomUpperCase);
         }
-        if(confirm('Would you like to include special characters?')) {
+        if (confirm('Would you like to include special characters?')) {
             choices.push(randomSpecialChar);
-        } 
+        }
     }
-    
+
     if (choices.length == 0) {
         alert('You must choose a valid option');
         return;
     }
 
+    var pass = [];
 
+    for (let i = 0; i < start; i++) {
+        let selected = choices[Math.floor(Math.random() * choices.length)];
+        pass.push(selected());
+    }
 
+    var passW = pass.join('');
+    return passW;
 }
 
+console.log(generatePassword());
+
+//random lower case letters
+function randomLowerCase() {
+    let lower = 'abcdefghijklmnopqrstuvwxyz';
+    return lower[Math.floor(Math.random() * lower.length)];
+}
+//random uppercase from character code
+function randomUpperCase() {
+    let upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return upper[Math.floor(Math.random() * upper.length)];
+}
+//random number from character code
+function randomNumber() {
+    let number = '0123456789';
+    return number [Math.floor(Math.random() * number.length)];
+}
+// random special char from character code
+function randomSpecialChar() {
+    let special = "!@#$%^&*()_-><[]}{";
+    return special[Math.floor(Math.random() * special.length)];
+}
 
 
 // Add event listener to generate button
